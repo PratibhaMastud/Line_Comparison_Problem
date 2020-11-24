dcf09e6c20bf6c2fd7e93ad9fc8b3aa0115f0026
PratibhaMastud <karandepratibha999@gmail.com>
2020-11-24 15:58:03 +0530

    Added:calculate length of line using cartecian system.

--- LineComparisonProblem/src/Problem/LineComparison.java
+++ LineComparisonProblem/src/Problem/LineComparison.java
@@ -1,10 +1,35 @@
-package Problem;
-
-public class LineComparison {
-
-	public static void main(String[] args) {
-
-		System.out.println("Welcome to Line Comparison Computation");
+	package Problem;
+	
+	import java.util.Scanner;
+	
+	public class LineComparison {
+	
+		public static void main(String[] args) {
+	
+			int firstX,firstY,secondX,secondY; 
+		    double lengthOfLine;
+	
+		    Scanner userInput=new Scanner(System.in);
+	
+		    System.out.println("Enter the length of X1");
+	
+		    firstX=userInput.nextInt();
+	
+		    System.out.println("Enter the length of Y1");
+	
+		    firstY=userInput.nextInt();
+	
+		    System.out.println("Enter the length of Y2");
+	
+		    secondY=userInput.nextInt();
+	  
+		    System.out.println("Enter the length of X2");
+	
+	        secondX=userInput.nextInt();
+	
+	        lengthOfLine=Math.sqrt((secondX-firstX)^2+(secondY-firstY)^2);
+	
+	        System.out.println("length of line is : "+ lengthOfLine);
+		}
+	
 	}
-
-}
